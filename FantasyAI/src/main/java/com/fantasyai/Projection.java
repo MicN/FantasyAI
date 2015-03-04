bd6577fafb946be547545547beea3334eafeb64e
Micah <nobody@nowhere.net>
2015-03-03 19:53:00 -0500

    Initial commit.

---
+++ FantasyAI/src/main/java/com/fantasyai/Projection.java
@@ -0,0 +1,5 @@
+package com.fantasyai;
+
+public interface Projection {
+
+}
---
+++ FantasyAI/src/main/java/com/fantasyai/Team.java
@@ -0,0 +1,7 @@
+package com.fantasyai;
+
+import java.util.Collection;
+
+public class Team {
+	Collection<Player> players;
+}
